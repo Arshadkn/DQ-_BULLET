7b970b4d1db92e5f8e83294ac675d96c6720c30d
Ak ser <133229616+AKSER256@users.noreply.github.com>
2023-07-17 22:02:05 +0530

    Update password.py

--- plugins/more/password.py
+++ plugins/more/password.py
@@ -12,5 +12,5 @@ async def password(bot, update):
     limit = int(qw)
     random_value = "".join(random.sample(password, limit))
     txt = f"<b>Limit:</b> {str(limit)} \n<b>Password: <code>{random_value}</code>"
-    btn = InlineKeyboardMarkup([[InlineKeyboardButton('⚡️ʙᴏᴛ ᴜᴩᴅᴀᴛᴇꜱ⚡️', url='https://t.me/+e_UqPGvuQ5E5NGU1')]])
+    btn = InlineKeyboardMarkup([[InlineKeyboardButton('⚡️ʙᴏᴛ ᴜᴩᴅᴀᴛᴇꜱ⚡️', url='https://t.me/moviehub_2k24')]])
     await message.edit_text(text=txt, reply_markup=btn, parse_mode=enums.ParseMode.HTML)
