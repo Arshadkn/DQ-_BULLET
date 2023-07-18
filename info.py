e9ec0a6e69a916c2246baa3b24bf45e31ec26e0e
Arshadkn <93204996+Arshadkn@users.noreply.github.com>
2023-07-18 17:13:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,6 @@
 import re
 from os import environ
 from Script import script 
-from time import time
-
 
 id_pattern = re.compile(r'^.\d+$')
 def is_enabled(value, default):
@@ -14,45 +12,44 @@ def is_enabled(value, default):
         return default
 
 # Bot information
-SESSION = environ.get('1BVtsOGYBu8Bp0RIgSVD86_Oj7CIZXj02hO2DybMUZfaAQR52W45OEKtHeJLgKvsLTf_tL9eLJUUdfVkE0zGDhm39QMZ1zBEpHUTBrpXlMMBz2lX7kdvEG9GykGE2VwzaUCbliZE2j7omi0jos6rO1nnNwG5pYybrDL_4HZG21G7DYD6fo2UgmWEZUOvar1bcj49vcCQNddQwQLNMjaZ_tsTpH9chQM0xxaKYVMq4IqM4gKGhGCVttTNVudMBQAIKxbqNTsxw8nKG786c-TtzyLKlhNBLTriWcWvYH07cacac2wXiNw70BPD-XKfasESM76dltGd7lCsUsiUnrmXRB2tGSYt-bYA=', 'Media_search')
-API_ID = int(environ['23189612'])
-API_HASH = environ['71ed204544e0715e17ea033e0aca2d6f']
-BOT_TOKEN = environ['5941529620:AAGWMi7yCRJi4mH6bpo5GuKe-ZQ8NS_xyb8']
-
-#rename
-FLOOD = int(environ.get("FLOOD", "10"))
-RENAME_MODE = bool(environ.get("RENAME_MODE"))
+SESSION = environ.get('SESSION', 'Media_search')
+API_ID = int(environ['API_ID'])
+API_HASH = environ['API_HASH']
+BOT_TOKEN = environ['BOT_TOKEN']
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
-PICS = (environ.get('PICS', 'https://graph.org/file/8142d603291ff2f556daf.jpg https://graph.org/file/e77e1aeca310e33d0c653.jpg https://graph.org/file/17914bf60e095f234063d.jpg https://graph.org/file/188d081c927d4a94edccb.jpg https://graph.org/file/2dac21855207ae35fdc3b.jpghttps://graph.org/file/85b6490b3dc7eb9a424c4.jpg https://graph.org/file/162347d5f0ebbd7a283fc.jpg')).split()
-NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/8142d603291ff2f556daf.jpg")
-NEWGRP = environ.get("NEWGRP", "https://graph.org/file/8142d603291ff2f556daf.jpg https://graph.org/file/e77e1aeca310e33d0c653.jpg https://graph.org/file/17914bf60e095f234063d.jpg https://graph.org/file/188d081c927d4a94edccb.jpg https://graph.org/file/2dac21855207ae35fdc3b.jpghttps://graph.org/file/85b6490b3dc7eb9a424c4.jpg https://graph.org/file/162347d5f0ebbd7a283fc.jpg")
-MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/file/91bd6cd35cb853bef9baa.mp4")
-SPELL_IMG = environ.get("https://graph.org/file/fbe7ce3c72b4fbcb92317.jpg", "https://graph.org/file/603fea39b4125eb257f22.jpg")
-SP = (environ.get('SP', '')).split()
+PICS = (environ.get('PICS', 'https://telegra.ph/file/ac524f66eec140ec69db5.jpg https://telegra.ph/file/bd94ba94b8bd34793cc81.jpg https://telegra.ph/file/0fd19a01130a1dec30ee2.jpg https://telegra.ph/file/9e71045e92e89ecf0b1cd.jpg https://telegra.ph/file/f739b561482df0be0a644.jpg')).split()
+NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/file/46443096bc6895c74a716.jpg")
+NEWGRP = environ.get("NEWGRP", "https://telegra.ph/file/732a9f89be5a9cd63289b.jpg")
+MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/file/f7f2a532fe4b990044507.mp4")
+SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/5e2d4418525832bc9a1b9.jpg")
+SP = (environ.get('SP', 'https://telegra.ph/file/db018384d5d139f3844ed.jpg https://telegra.ph/file/30c736c93b5ad5c328141.jpg https://telegra.ph/file/f1565e213ec1a45a27362.jpg https://telegra.ph/file/0c53da8c1598c63e50a6e.jpg https://telegra.ph/file/360d78cf3209429ca8e66.jpg')).split()
 
 
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('5431311680', '6118927515').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('-1001932781121', '0').split()]
+ADMIN = int(environ.get('ADMINS'))
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '0').split()]
 
 
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('5431311680', '6118927515').split()]
+auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_channel = environ.get('-1001932781121')
-auth_grp = environ.get('-1001900195275')
-AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(-1001932781121) else None
+auth_channel = environ.get('AUTH_CHANNEL')
+auth_grp = environ.get('AUTH_GROUP')
+AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
-support_chat_id = environ.get('-1001900195275')
-reqst_channel = environ.get('-1001932781121')
+support_chat_id = environ.get('SUPPORT_CHAT_ID')
+reqst_channel = environ.get('REQST_CHANNEL_ID')
 REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
-login_channel = environ.get('-1001932781121')
+login_channel = environ.get('LOGIN_CHANNEL')
 LOGIN_CHANNEL = int(login_channel) if login_channel and id_pattern.search(login_channel) else None
+pm = environ.get('PM')
+PM = int(pm) if pm and id_pattern.search(pm) else None
 
 
 # MongoDB information
@@ -61,8 +58,8 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Rajappan")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Custom Chats
-FILE_CHANNEL = int(environ.get('FILE_CHANNEL', ''))
-FILE_CHANNEL_LINK = environ.get('FILE_CHANNEL_LINK', '')
+FILE_CHANNEL = int(environ.get('FILE_CHANNEL', '-1001616308548'))
+FILE_CHANNEL_LINK = environ.get('FILE_CHANNEL_LINK', 'https://t.me/TGxMULTIBOTDB')
 
 # This is required for the plugins involving the file system.
 TMP_DOWNLOAD_DIRECTORY = environ.get("TMP_DOWNLOAD_DIRECTORY", "./DOWNLOADS/")
@@ -88,12 +85,11 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 MAX_B_TN = environ.get("MAX_B_TN", "7")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "False")), False)
 PORT = environ.get("PORT", "8080")
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+8vZTQtzo0lBmNDY9')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+e_UqPGvuQ5E5NGU1')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/NasraniSeries')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/bigmoviesworld')
 MSG_ALRT = environ.get('MSG_ALRT', 'Piracy Is Crime')
-NOR_ALRT =  environ.get('NOR_ALRT', 'NO IMAGES IS FOUND')
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Nothing_Support_Group')
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'NasraniSeries')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), True)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
 AUTO_FFILTER = is_enabled((environ.get('AUTO_FFILTER', "True")), True)
@@ -103,7 +99,7 @@ CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CUSTOM_FILE_C
 CUSTOM_QUERY_CAPTION = environ.get("CUSTOM_QUERY_CAPTION", f"{script.CUSTOM_QUERY_CAPTION}")
 CAPTION = environ.get("CAPTION", f"{script.CAPTION}")
 BR_IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", f"{script.BR_TEMPLATE_TXT}")
-BATCH_LINK = environ.get('BATCH_LINK',"")
+BATCH_LINK = environ.get('BATCH_LINK',"https://t.me/nasrani_update")
 BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", f"{script.CUSTOM_FILE_CAPTION}")
 IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", f"{script.IMDB_TEMPLATE_TXT}")
 LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
@@ -116,8 +112,8 @@ PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
 
 #redict
-MAIN_CHANNEL = environ.get('MAIN_CHANNEL',"https://t.me/Royal_Enfield_bot_Updates")
-FILE_FORWARD = environ.get('FILE_FORWARD',"https://t.me/+hHHW8TOqFhE1OWQ1")
+MAIN_CHANNEL = environ.get('MAIN_CHANNEL',"https://t.me/nasrani_update")
+FILE_FORWARD = environ.get('FILE_FORWARD',"https://t.me/+7oxSIxY4X0c2ZGVl")
 MSG_ALRT = environ.get('MSG_ALRT', '𝑪𝑯𝑬𝑪𝑲 & 𝑻𝑹𝒀 𝑨𝑳𝑳 𝑴𝒀 𝑭𝑬𝑨𝑻𝑼𝑹𝑬𝑺')
 FILE_CHANNEL = int(environ.get('FILE_CHANNEL', 0))
 
